52216138a065fcea42a36a76869e12c150eee4d9
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-10-27 13:22:26 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.getElementById("addToCart").addEventListener("dblclick", function() {
+    alert("Item added to cart!");
+});
+
+document.getElementById("buyNow").addEventListener("click", function() {
+    alert("Proceeding to checkout!");
+});
