3093898e55457a7494ce5d0839619cf4eed80006
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-11-08 09:32:42 +0530

    HTML | Tables

---
+++ HTML/Tables/Theory.js
@@ -0,0 +1,40 @@
+/*
+WHat is Table:
+A table is a structured arrangement of data made up of rows and columns. It is used to organize and present information in a clear and concise manner, making it easier to read and understand. Tables are commonly used in various fields such as data analysis, research, and business to display numerical data, comparisons, and relationships between different variables.
+
+use cases:
+1. Student data
+2. Timetables
+3. Courses LIst
+4. Employee data
+5. Product listings
+6. Financial reports
+
+Basic Structure of Table:
+<table></table>--->defines the table
+<tr></tr>--->defines the table row
+<th></th>--->defines the table header
+<td></td>--->defines the table data/cell
+<thead></thead>--->defines the table header section
+<tbody></tbody>--->defines the table body section
+<tfoot></tfoot>--->defines the table footer section
+
+table attributes:
+border--->specifies the border width of the table
+cellpadding--->specifies the space between cell content and cell border
+cellspacing--->specifies the space between cells
+colspan--->specifies the number of columns a cell should span
+rowspan--->specifies the number of rows a cell should span
+
+
+table properties:
+text-align--->specifies the alignment of the table
+width--->specifies the width of the table
+height--->specifies the height of the table
+border-collapse--->specifies whether the table borders should collapse into a single border or be separated
+border: specifies the  width,style, and color of the table borders
+
+
+
+
+*/
