3b88c9a5f6c5c8fe3c17d3ce42d018d8133246f1
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-11-10 12:58:41 +0530

    forms

---
+++ HTML/Forms/theory.js
@@ -0,0 +1,15 @@
+/*
+
+What is form:
+from is uded to collect data from user and store in backend
+
+Why we form:
+1. to collect data from User like name,email,age,feedback
+2. to submit the data
+3. sisn in or sign up from to collect and to access the appplication
+
+Synatrx:
+<form>\
+        input
+</form>
+*/
