057d43b8984618a1258b4c59bd1efc834eb0d7a7
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-11-19 17:12:39 +0530

    Selectors part-2

---
+++ CSS/intro/Selectors.js
@@ -0,0 +1,24 @@
+/*
+WHat is Selector:
+
+Selector help us to target a specific element in our HTML document
+
+WHy:
+1. make easier to maintain the code and update the styles
+2. increase the efficiency of code
+3. aloow for clean and reusable
+
+syntax: 
+selctor{
+    po[roperty-name :value;
+}
+
+
+types of Selectors:
+1. Global Selector/ Universal Selector(*)
+2. Element/tag Seleg selector
+    selects the element of the specific tag
+3. class Selector(.)
+4. ID Selector(#)
+5. Group Selector(,)
+*/
