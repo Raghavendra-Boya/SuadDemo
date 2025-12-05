eea8efe73ce16566181447f6502d5114ec815d67
Boya-Raghavendra <raghavendra.achieversit@gmail.com>
2025-12-05 09:45:27 +0530

    Layout | Grid |Flex

---
+++ CSS/intro/flex/script.js
@@ -0,0 +1,13 @@
+/*
+WHat is Flex:
+
+1. flex is one dimensional layout method to disply the elements in row or column direction
+2. flex default directi0on is row/horizantal
+3. this is suitable for responsive web pages
+
+
+flex 2 types:
+1. flex container:(parent)
+2. flex items:(child)
+
+*/
